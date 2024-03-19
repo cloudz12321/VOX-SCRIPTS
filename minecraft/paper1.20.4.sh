4fd1eb2cd9c1e355622170353d872b6eb29d3011
Mihai2009 <158510509+cloudz12321@users.noreply.github.com>
2024-03-19 10:59:01 +0200

    Update paper1.20.4.sh

--- minecraft/paper1.20.4.sh
+++ minecraft/paper1.20.4.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-    echo "Downloading Paper 1.20.4..."
-    curl -o "paper1.20.4.jar" "https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/454/downloads/paper-1.20.4-454.jar"
-    echo "Download of Paper 1.20.4 complete."
-    sleep 2
+echo "Downloading Paper 1.20.4..."
+mkdir PAPER-SERVER
+sleep 3
+cd PAPER-SERVER
+sleep 3
+curl -o "paper1.20.4.jar" "https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/454/downloads/paper-1.20.4-454.jar"
+sleep 3
+echo "Download of Paper 1.20.4 complete."
+sleep 2
 echo "Now just type java -jar paper1.20.4.jar"
