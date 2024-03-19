d66003161b073f98c3d50d75706c9177df65386e
Mihai2009 <158510509+cloudz12321@users.noreply.github.com>
2024-03-19 10:53:31 +0200

    Create paper1.20.4.sh

---
+++ minecraft/paper1.20.4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+    echo "Downloading Paper 1.20.4..."
+    curl -o "paper1.20.4.jar" "https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/454/downloads/paper-1.20.4-454.jar"
+    echo "Download of Paper 1.20.4 complete."
+    sleep 2
+echo "Now just type java -jar paper1.20.4.jar"
